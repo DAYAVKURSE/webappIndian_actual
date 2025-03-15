5a0ea226280a3321335c94c3177ba53810e36983
high-cuisine <143965582+high-cuisine@users.noreply.github.com>
2025-03-15 18:37:57 +0400

    delete payment

--- site/blessed/src/requests/payments/createPaymentPage.js
+++ site/blessed/src/requests/payments/createPaymentPage.js
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 const initData = window.Telegram.WebApp.initData;
 
 export async function createPaymentPage(amount) {
+    if(amount > 0) return;
     try {
         const response = await fetch(`https://${API_BASE_URL}/payments/create`, {
             method: 'POST',
