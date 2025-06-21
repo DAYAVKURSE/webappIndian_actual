7fb4186bb71c2ef93e17fa021f7baf9ed3832200
asimakov <asimakov@standfin.ru>
2025-06-21 21:42:35 +0300

    актуальная версия

--- site/blessed/src/config.js
+++ site/blessed/src/config.js
@@ -2,5 +2,5 @@ const isDev = import.meta.env.DEV;
 
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const API_BASE_URL = isDev
-  ? 'http://testfakeserver.com/api'   // dev-сервер
-  : 'http://testfakeserver.com/api';  // продакшен
+  ? 'https://testfakeserver.com/api'   // dev-сервер
+  : 'https://testfakeserver.com/api';  // продакшен
