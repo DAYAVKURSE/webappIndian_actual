1bd824fe1df5a66754d2f5df1c1d3d0544c8702c
asimakov <asimakov@standfin.ru>
2025-07-15 18:55:25 +0300

    баг фикс

--- site/blessed/src/config.js
+++ site/blessed/src/config.js
@@ -1,8 +1,6 @@
-const isDev = import.meta.env.DEV;
 
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 
-export const API_BASE_URL = isDev
-  ? 'rupex.io'
-  : 'rupex.io';
+export const API_BASE_URL = 'https://rupex.io';
+
