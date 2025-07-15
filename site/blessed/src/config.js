e60bd9171c00cae32a61e93db804d1da55c7f303
asimakov <asimakov@standfin.ru>
2025-07-15 19:31:47 +0300

    фикс

--- site/blessed/src/config.js
+++ site/blessed/src/config.js
@@ -2,5 +2,5 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 
-export const API_BASE_URL = 'https://rupex.io';
+export const API_BASE_URL = 'https://rupex.io/api';
 
